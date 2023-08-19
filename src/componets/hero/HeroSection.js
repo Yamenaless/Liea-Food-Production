4763b09c93ac7e6c93514a73ed3aa7062d4029cd
Yamen <yamenaless@gmail.com>
2023-08-19 03:26:39 +0300

    first commit

---
+++ src/componets/hero/HeroSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="w-full h-[100vh]">
+      <img 
+        className="w-full h-[100vh] bg-cover"
+        src="https://uploads-ssl.webflow.com/63c0348e8e38eb4cc747e99e/6458c2c39039924ac9f2319a_Liea-hero-image.jpg"
+      alt="background"></img>
+    </div>
+  );
+};
+
+export default HeroSection;
